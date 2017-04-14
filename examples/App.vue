ac2ea399da952266461af26db5d2e8d40b6bf413
yangyue <yangyue0372@outlook.com>
2017-04-14 11:20:11 +0800

    Init

---
+++ examples/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h2>This is Vue Markdown Editor</h2>
+    <div class="vmd-container">
+      <vue-editor v-model="value" @input="input"></vue-editor>
+      <vue-editor></vue-editor>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        value: '# This is parent data.'
+      }
+    },
+    methods: {
+      input() {
+        console.log(this.value)
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  #app > h2 {
+    text-align: center;
+  }
+
+  .vmd-container {
+    margin: 0 auto;
+    width: 800px;
+    height: 600px;
+  }
+</style>
---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by anguer on 2017/4/11.
+ */
+import VueMarkdownEditor from './components/main.vue';
+
+/* istanbul ignore next */
+VueMarkdownEditor.install = function(Vue) {
+  Vue.config.keyCodes.a = 65;
+  Vue.config.keyCodes.b = 66;
+  Vue.config.keyCodes.c = 67;
+  Vue.config.keyCodes.d = 68;
+  Vue.config.keyCodes.h = 72;
+  Vue.config.keyCodes.i = 73;
+  Vue.config.keyCodes.l = 76;
+  Vue.config.keyCodes.o = 79;
+  Vue.config.keyCodes.p = 80;
+  Vue.config.keyCodes.q = 81;
+  Vue.config.keyCodes.t = 84;
+  Vue.config.keyCodes.u = 85;
+  Vue.component(VueMarkdownEditor.name, VueMarkdownEditor);
+};
+
+export default VueMarkdownEditor;
